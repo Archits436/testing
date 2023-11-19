51e0468fd0bb9b34477b9b083623507fcbce84bc
Archits436 <archits436@gmail.com>
2023-11-19 13:49:00 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:testing/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,13 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome To My App"),
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
